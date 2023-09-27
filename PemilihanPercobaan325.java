cd3d4ad4fea1d4381bf749f61f441e83fbc0cb00
romy081216369646 <49948685+romy081216369646@users.noreply.github.com>
2023-09-27 10:00:57 +0700

    Add files via upload

---
+++ PemilihanPercobaan325.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan325 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input.nextDouble();
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input.nextDouble();
+        System.out.print("Masukkan operator (* - + /):" );
+        operator = input.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println(angka1 + "+" + angka2 + "=" + hasil);
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println(angka1 + "-" + angka2 + "=" + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println(angka1 + "*" + angka2 + "=" + hasil);
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                System.out.println(angka1 + "/" + angka2 + "=" + hasil);
+                break;
+            default:
+                System.out.println("Operator tidak valid");
+        }
+    }
+}
